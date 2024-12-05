b38ef8572521ae9dafa708eda2be7b29b6c3e3e3
Seturaj <seturajmatroja@gmail.com>
2024-12-05 15:47:25 -0500

    Added Components and Intervals

--- server/index.js
+++ server/index.js
@@ -7,22 +7,25 @@ const PORT = process.env.PORT || 8000;
 app.use(cors()); // Enable CORS for all requests
 
 // Configuration: time in milliseconds before status changes
-const CONFIGURED_TIME = 50000; // 5 seconds
+const CONFIGURED_TIME = 10000; // 10 seconds
 let startTime = Date.now(); // Record the start time
+
 
 // Status API
 app.get('/status', (req, res) => {
-  const elapsedTime = Date.now() - startTime;
-  if (elapsedTime < CONFIGURED_TIME) {
-    res.json({ result: "pending" });
-  } else {
-    let num = Math.random();
-    console.log(num);
-
-    const status = num > 0.5 ? "completed" : "error"; 
-    res.json({ result: status });
-  }
-});
+    setTimeout(() => {
+      const elapsedTime = Date.now() - startTime;
+      if (elapsedTime < CONFIGURED_TIME) {
+        res.json({ result: "pending" });
+      } else {
+        let num = Math.random();
+        console.log(num);
+  
+        const status = num > 0.5 ? "completed" : "error"; 
+        res.json({ result: status });
+      }
+    }, 5000); // Wait for 5 seconds before processing the request
+  });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
